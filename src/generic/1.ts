13c0f0460f9d344c5f6faf1576de31ac32137ed4
Nazar Smutnitskiy <nsmutnitskiy@swivl.com>
2024-02-18 20:11:56 +0200

    add hw-06

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -1,17 +1,14 @@
-/*
-  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
-  Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
-*/
+type DataType = [string, number];
 
-function getPromise () {
-  return new Promise((resolve) => {
-    resolve(['Text', 50]);
-  });
+function getPromise(): Promise<DataType> {
+    return new Promise<DataType>((resolve) => {
+        resolve(['Text', 50]);
+    });
 }
 
 getPromise()
-.then((data) => {
-  console.log(data);
-});
+    .then((data) => {
+        console.log(data);
+    });
 
 export {};
